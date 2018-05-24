ae45d11db3b70cd3e65dc416ee72b277c6570ba7
Ragnar-BY <roman.buzuk@gmail.com>
2018-05-24 11:57:28 +0300

    Added handler test for AddPlayerHandler

--- pkg/server/handlers_test.go
+++ pkg/server/handlers_test.go
@@ -5,25 +5,35 @@ import (
 	"net/http/httptest"
 	"testing"
 
+	"errors"
+
 	"github.com/Ragnar-BY/gamingwebsite_testtask/pkg/manager"
 	"github.com/gavv/httpexpect"
 	"github.com/gorilla/mux"
 )
 
 func TestManagerRouter_AddPlayerHandler(t *testing.T) {
-
 	db := &manager.MockDB{}
-	m := newManagerRouter(manager.Manager{db}, mux.NewRouter())
+	m := newManagerRouter(manager.Manager{DB: db}, mux.NewRouter())
 
 	server := httptest.NewServer(m)
 	defer server.Close()
-
-	db.On("AddPlayer", "player1").Return(1, nil)
 	e := httpexpect.New(t, server.URL)
 
-	e.Request(http.MethodPost, "/add").WithQuery("name", "player1").
-		Expect().Status(http.StatusCreated).JSON().Number().Equal(1)
-
+	t.Run("Success", func(t *testing.T) {
+		db.On("AddPlayer", "player1").Return(1, nil)
+		e.Request(http.MethodPost, "/add").WithQuery("name", "player1").
+			Expect().Status(http.StatusCreated).JSON().Number().Equal(1)
+	})
+	t.Run("WrongName", func(t *testing.T) {
+		e.Request(http.MethodPost, "/add").WithQuery("name", "").
+			Expect().Status(http.StatusBadRequest).
+			Body().Contains("wrong name") // do we need to check error message or only status?
+	})
+	t.Run("DBError", func(t *testing.T) {
+		db.On("AddPlayer", "player2").Return(0, errors.New("cannot add new player"))
+		e.Request(http.MethodPost, "/add").WithQuery("name", "player2").
+			Expect().Status(http.StatusBadRequest) //same question as above
+	})
 	db.AssertExpectations(t)
-
 }
