49e9440416674124a820c76173f7b8d0be0d4070
Ragnar-BY <roman.buzuk@gmail.com>
2018-05-25 11:03:02 +0300

    Added handler test for balancePlayerHandler

--- pkg/server/handlers_test.go
+++ pkg/server/handlers_test.go
@@ -8,6 +8,7 @@ import (
 	"errors"
 
 	"github.com/Ragnar-BY/gamingwebsite_testtask/pkg/manager"
+	"github.com/Ragnar-BY/gamingwebsite_testtask/pkg/player"
 	"github.com/gavv/httpexpect"
 	"github.com/gorilla/mux"
 )
@@ -28,7 +29,7 @@ func TestManagerRouter_AddPlayerHandler(t *testing.T) {
 	t.Run("WrongName", func(t *testing.T) {
 		e.Request(http.MethodPost, "/add").WithQuery("name", "").
 			Expect().Status(http.StatusBadRequest).
-			Body().Contains("wrong name") // do we need to check error message or only status?
+			Body().Contains("wrong name") // TODO:do we need to check error message or only status?
 	})
 	t.Run("DBError", func(t *testing.T) {
 		db.On("AddPlayer", "player2").Return(0, errors.New("cannot add new player"))
@@ -36,4 +37,38 @@ func TestManagerRouter_AddPlayerHandler(t *testing.T) {
 			Expect().Status(http.StatusBadRequest) //same question as above
 	})
 	db.AssertExpectations(t)
+}
+
+func TestManagerRouter_balancePlayerHandler(t *testing.T) {
+	db := &manager.MockDB{}
+	m := newManagerRouter(manager.Manager{DB: db}, mux.NewRouter())
+	server := httptest.NewServer(m)
+	defer server.Close()
+	e := httpexpect.New(t, server.URL)
+
+	t.Run("Success", func(t *testing.T) {
+		db.On("PlayerByID", 1).Return(&player.Player{
+			ID:      1,
+			Balance: 1.5,
+		}, nil)
+		e.Request(http.MethodGet, "/balance/1").
+			Expect().Status(http.StatusOK).JSON().Number().Equal(1.5)
+	})
+
+	t.Run("DBError", func(t *testing.T) {
+		db.On("PlayerByID", 2).Return(nil, errors.New("some error"))
+		e.Request(http.MethodGet, "/balance/2").
+			Expect().Status(http.StatusBadRequest)
+	})
+
+	t.Run("PlayerParseIDError", func(t *testing.T) {
+		e.Request(http.MethodGet, "/balance/98765432109876543210").
+			Expect().Status(http.StatusBadRequest)
+	})
+	//TODO: do we need to check such cases?
+	t.Run("PlayerWrongIDError", func(t *testing.T) {
+		e.Request(http.MethodGet, "/balance/wrongid").
+			Expect().Status(http.StatusNotFound)
+	})
+
 }
